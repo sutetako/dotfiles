349fe19a40d981a2a959a9d9739f76d52294e22c
Moto Endo <endo.moto.biz@gmail.com>
2019-05-22 22:41:16 +0900

    update configure shell

--- vim.configure.sh
+++ vim.configure.sh
@@ -1,1 +1,2 @@
-configure --with-features=huge --enable-gui=gtk3 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --with-luajit --enable-fail-if-missing
+# copy to root directory of vim project
+./configure --with-features=huge --enable-gui=gtk3 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --with-luajit --enable-fail-if-missing
