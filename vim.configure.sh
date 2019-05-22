a30891bc746386fd03aeb9204d53523409b58bc7
Moto Endo <endo.moto.biz@gmail.com>
2019-05-22 22:34:02 +0900

    configure settings for vim8

---
+++ vim.configure.sh
@@ -0,0 +1,1 @@
+configure --with-features=huge --enable-gui=gtk3 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --with-luajit --enable-fail-if-missing
