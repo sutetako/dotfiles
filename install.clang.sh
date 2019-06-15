cd7aee3e835ffd6336d785ba1e67156d25da4fd6
Moto Endo <endo.moto.biz@gmail.com>
2019-06-15 16:38:33 +0900

    add alternatives

--- install.clang.sh
+++ install.clang.sh
@@ -2,4 +2,6 @@ sudo echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >> /etc
 sudo echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >> /etc/apt/sources.list.d/clang.list
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt update && sudo apt install  clang-8 lldb-8 lld-8 clang-tools-8
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-8 100
 
